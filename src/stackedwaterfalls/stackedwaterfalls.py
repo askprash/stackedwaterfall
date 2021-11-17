5b69e459d9998a98c3e3e7719261f15774ffb2e3
Prashanth <prash@mit.edu>
2021-11-17 00:10:38 -0500

    Edits to xlabel and legend

--- src/stackedwaterfalls/stackedwaterfalls.py
+++ src/stackedwaterfalls/stackedwaterfalls.py
@@ -7,7 +7,7 @@ import itertools
 
 class StackedWaterfalls():
     
-    def __init__(self, data, datalabels = [], datacolors = [], barlst = []):
+    def __init__(self, data, datalabels = [], datacolors = []):
         
         self.rawdata = data
         self.nbars = len(data)
@@ -19,7 +19,7 @@ class StackedWaterfalls():
         
         self.colors = datacolors
         
-        self.barlst = barlst
+        self.barlst = []
         self.namelst = []
         
         
@@ -49,18 +49,25 @@ class StackedWaterfalls():
     
     def stdcolors(self):
         n = len([x for x in list(itertools.chain(*self.rawdata))])
-        phi = 0.618033988749895 #irrational number to use per equidistribution theorem, 
-        # and supposedly the golden ratio is the best acc. to Fibonacci hashing.
-        
+        phi = 0.618033988749895         
         colors = [to_hex(hsv_to_rgb([(i * phi) % 1.0,0.7, np.sqrt(1.0 - (i*phi)%0.5)]))
                   for i in range(n)]
         return colors
             
-    def plot(self, ax=None, xstart = 0, total=True, edgecolor = None, barwidth = 0.2, gap = 0.2,
-             plotlinks = True,  totalcolor = 'dimgrey', shadecolor = 'None'):
+    def plot(self, ax=None, xstart = 0, total=False, edgecolor = None, barwidth = 0.2, gap = 0.2, barkw={},
+             plotlinks = True, linkskw={},  totalcolor = 'dimgrey', shadecolor = 'None', dropax = 0, legend = True, legkw={},
+             grouplabel = None, grouplabelstyle = "|", grouplabelkw={}, barnames=[]):
+        
         
         self.barwidth = barwidth
         self.gap = gap
+        self.total = total
+        self.barlst = []
+        self.namelst = []
+        
+        self.prep_data()
+        self.arrange_labels()
+        self.arrange_colors()
         
         if ax is None:
             fig, ax = plt.subplots(dpi = 100, figsize = (8,5))
@@ -72,6 +79,7 @@ class StackedWaterfalls():
 
         if total:
             xtotal = xstart
+            self.xtotal = xtotal
             totalval = self.df['Total'].sum()
             self.barlst.append(ax.bar(x = xtotal, height = totalval,
                                  width = barwidth, color = totalcolor, edgecolor = ecolor,
@@ -80,8 +88,11 @@ class StackedWaterfalls():
             xstart = xstart+barwidth+gap #offset start to accomodate total bar
         
         xlocs = np.linspace(xstart, xstart+(self.nbars-1)*(barwidth+gap), self.nbars)
+        self.xlocs = xlocs
         
         cindexstart = 0
+#         barkw['linewidth']=barkw.get('linewidth',0.5)
+        
         prev_lev = [0]*self.nbars
         for lev in self.levnames:
             if self.colors:
@@ -94,9 +105,11 @@ class StackedWaterfalls():
             if edgecolor is None:
                 ecolor = colors
             else:
-                ecolor = edgecolor    
+                ecolor = edgecolor 
+                
+#             barkw['edgecolor']=barkw.get('edgecolor',ecolor)
             self.barlst.append(ax.bar(x = xlocs, height = self.df[lev], bottom = self.df['Bottom']+prev_lev,
-                                 width = barwidth,color = colors , edgecolor = ecolor, linewidth = 0.5, zorder = 10))
+                                 width = barwidth,color = colors , edgecolor = ecolor, linewidth = 0.5, zorder = 10, **barkw))
             self.namelst.append(self.dfnames[lev].tolist())
             prev_lev = prev_lev + self.df[lev]
         
@@ -105,19 +118,97 @@ class StackedWaterfalls():
             link_lines[1:-1:3] = np.nan
             link_lines = link_lines[1:-1]
             xs = [y + barwidth/2 if x%2==0 else y-barwidth/2 for (x,y) in zip(itertools.count(),(xlocs.repeat(3)[1:-1]))]
-
-            ax.plot(xs, link_lines, lw = 0.5, ls = '--', color = 'k', zorder = 1)
+            linkskw['color'] = linkskw.get('color', 'k')
+            linkskw['ls']    = linkskw.get('ls', '--')
+            linkskw['lw']    = linkskw.get('lw', '0.5')
+            ax.plot(xs, link_lines, **linkskw, zorder = 1)
             
         xshadestart = xlocs[0] - gap
         xshadeend   = xlocs[-1] + gap
         
         if total:
-            ax.plot([xtotal, xlocs[-1]], [totalval, totalval], lw = 0.5, ls = '--', color = 'k', zorder = 1)
+            ax.plot([xtotal, xlocs[-1]], [totalval, totalval], **linkskw, zorder = 1)
             xshadestart = xtotal - gap
 
         shade = ax.axvspan(xshadestart, xshadeend, facecolor = shadecolor, alpha = 0.1, zorder = 0)
-            
+        
+        ax.set_xticks([])
+        self.xaxlabeler(ax, grouplabel, barnames, grouplabelstyle)
+               
+        if legend:
+            self.legendcreator(ax, legkw)
+
         return ax
+    
+    def legendcreator(self, ax, legkw):
+        rects = [x for x in list(itertools.chain(*self.barlst)) if x.get_height() != 0] # get only those rects that are non-zero heights
+        labels = [x for x in list(itertools.chain(*self.namelst)) if x != 0]
+
+        l = ax.legend(rects, labels, **legkw)
+        l.set_zorder(30)
+ 
+    
+    def xaxlabeler(self, ax, grouplabel, barnames, grouplabelstyle):
+        
+        fs = ax.xaxis.label.get_size()
+        # Set these in axes coords
+        bracketheight = 0.015
+        bracketoffset = 0.02
+        transx = ax.get_xaxis_transform()
+        
+        xstart = self.xtotal if self.total else self.xlocs[0]
+        xmid = (xstart + self.xlocs[-1])/2.0
+        
+        if barnames:
+            if self.total:
+                ax.set_xticks([self.xtotal] + list(self.xlocs))
+                ax.set_xticklabels(["Total"]+barnames)
+                
+            else:
+                ax.set_xticks(self.xlocs)
+                ax.set_xticklabels(barnames)
+                
+                
+            # Offset group labels by appropriate amount
+            plt.tight_layout() # need this to get fig rendered. Should be a better way perhaps
+            # get lower y lim
+            yminlabel = max([abs(x.get_window_extent().transformed(transx.inverted()).y0) for x in ax.get_xticklabels()])
+            bracketoffset = bracketoffset + yminlabel
+        
+        if grouplabel is not None:
+            ygrplabel = 0
+            
+            # Group label styles:
+            # |, ], ]-, |-
+            if grouplabelstyle is not None:
+                # endpt = datapt -+ gap +- offset
+                xlineA = xstart  - self.gap + self.gap/5 #Start of label line
+                xlineB = self.xlocs[-1] + self.gap - self.gap/5 #End of label line
+
+                # Use get_xaxis_transform() so that x coords are in data coords and y are in axes coords
+                ax.plot([xlineA, xlineB], [-bracketoffset,  -bracketoffset ], 
+                        transform = transx, color = 'k', clip_on=False, lw = 0.8)
+                ygrplabel = -bracketoffset-0.02
+
+            if (grouplabelstyle == ']') or (grouplabelstyle == ']-'):
+                ax.plot([xlineA, xlineA, np.nan, xlineB, xlineB],
+                        [-bracketoffset, -bracketoffset+bracketheight, np.nan, 
+                         -bracketoffset, -bracketoffset+bracketheight],
+                       transform = transx, color = 'k', clip_on=False, lw = 0.8)
+                ygrplabel = -bracketoffset-0.02
+
+            if (grouplabelstyle == ']-') or (grouplabelstyle == '|-'):
+
+                ax.plot([xmid, xmid],[-bracketoffset, -bracketoffset-bracketheight],
+                       transform = transx, color = 'k', clip_on=False, lw = 0.8)
+                ygrplabel = -bracketoffset-2*bracketheight
+
+
+            ax.annotate(grouplabel, xy=(xmid, ygrplabel), xycoords = transx, 
+                    ha='center', va='top', fontsize = fs,
+                    bbox=dict(boxstyle='square', fc='None',ec = 'None'))
+        
+
     
 
         
